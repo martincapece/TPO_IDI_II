5d6e127e8a57280ae4a8847c9a2796055cdd275e
PatricioVecino <patriciogabrielvecino@gmail.com>
2025-10-18 15:14:27 -0300

    model notificacion

---
+++ src/main/java/com/tpo/prisma/model/Notificacion.java
@@ -0,0 +1,53 @@
+package com.tpo.prisma.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Notificacion")
+public class Notificacion {
+
+    @Id
+    private String id;
+    
+    private String mensaje;
+    private String contentId;
+    private String creatorUser;
+
+    public Notificacion(String mensaje, String contentId, String creatorUser) {
+        this.mensaje = mensaje;
+        this.contentId = contentId;
+        this.creatorUser = creatorUser;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getContentId() {
+        return contentId;
+    }
+
+    public void setContentId(String contentId) {
+        this.contentId = contentId;
+    }
+
+    public String getCreatorUser() {
+        return creatorUser;
+    }
+
+    public void setCreatorUser(String creatorUser) {
+        this.creatorUser = creatorUser;
+    }
+}
